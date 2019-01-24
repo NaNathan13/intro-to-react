a2688bcb09d563aaf9b7c4110bfd17ab6dbca5f2
NaNathan13 <natewilson135@gmail.com>
2019-01-24 12:54:58 -0500

    completed react to do state and props checkpoint

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -4,7 +4,10 @@ class ToDo extends Component {
     render () {
         return (
             <div>
-                <li>a todo will go here</li>
+                <li>
+                    <input type="checkbox" checked={ this.props.isCompleted }/>
+                    <span>{ this.props.description }</span>
+                </li>
             </div>
         );
     }
